2da9144d4037ee9fd312fab18f70fd1ce875713b
devildummy <cyrok.13@gmail.com>
2018-10-02 19:25:22 +0300

    Ok, cant do it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function count(s, pairs) {
-  // your implementation
-}
+  let N=1;
+  let count=0;
+  for(let i=0, len=pairs.length; i<len;i++){
+    N*=Math.pow(pairs[i][0],pairs[i][1]);
+  }
+  if(!isFinite(N) || N>10000000) return 'Number is too large, sorry';
+  let gcd = (a, b) => {
+    if ( ! b) {
+        return a;
+    }
+
+    return gcd(b, a % b);
+};
+  for(let k=0; k<N;k++){
+    for(let j=0,len=s.length;j<len;j++){
+      if(s.charAt(j)=='1'){
+        if(gcd(k+j,N)!=1){
+          break;
+        }
+      }else{
+        if(gcd(k+j,N)==1){
+          break;
+        }
+      }if(j==len-1){
+        count++;
+      }
+    }
+  }
+  return count%1000000007;
+};
